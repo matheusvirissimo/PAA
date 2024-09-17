889153c9fc3c0c638a559451dd1b22c8d5958dc0
matheusvirissimo <matheus.virissimo@unesp.br>
2024-09-16 22:26:52 -0300

    refactor: mudando o nome do arquivo

--- bubblesort.py
+++ bubblesort.py
@@ -1,13 +1,16 @@
 def bubble_sort(array): ## recebe uma lista
 
     ## loop de fora para iterar a lista n vezes
+    ## for(int n = len(array); n > 0; n--)
     for n in range(len(array) - 1, 0, -1):
         for i in range(n):
+
+            # Compara o elemento atual com o próximo
             if array[i] > array[i+1]:
                 temp = array[i]
                 array[i] = array[i+1]
                 array[i+1] = temp 
-    return array
+    return array # não é necessário
 
 array = [39, 12, 18, 85, 72, 10, 2, 18]
 print("Array desorganizado")
